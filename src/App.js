865cd5e06cc7deeafe7561e81424b587907a6a97
04ezhil <ezhilarasan.csbe@gmail.com>
2025-11-01 17:47:27 +0530

    app version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import './todoList.js';
+import './didList.js';
+import TodoList from './todoList.js';
+import DidList from './didList.js';
+import { useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 function App() {
+  var list = ['wake up early','Do excersize'];
+  let [todoList,setTodoList] = useState([]);
+  let [didList,setDidList] = useState([]);
+  
+
+  let add = (what) => {
+    setTodoList([...todoList,what]);
+  }
+  let addHist =(did) => {
+    setTodoList(todoList.filter((i) => i !== did));
+    setDidList([...didList,did]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <nav className='navbar p-4 bg-indigo'>
+      <div className='container-fluid'><h1 className='text-white'>TodoList</h1></div> 
+    </nav>
+    <TodoList todos={todoList} addlist={add} addHistory={addHist}/>
+    <hr/>
+    <nav className='navbar p-4 bg-indigo-dark'>
+      <div className='container-fluid'><h1 className='text-white'>Tasks Completed</h1>
+      <form className="d-flex">
+        <button className="btn btn-primary ps-4 pe-4" type="button" onClick={()=>setDidList([])}>clear</button>
+      </form>
+      </div> 
+    </nav>
+    <DidList didList={didList}/>
+    
+    </>
   );
 }
 
 export default App;
+
+
+
+
+
